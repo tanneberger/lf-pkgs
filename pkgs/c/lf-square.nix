0c430cafd8f5488c236043b4031e16abb12fcdff
revol-xut <revol-xut@protonmail.com>
2022-06-30 10:31:45 +0200

    lf-square: 0.1.0 -> 0.1.1

--- pkgs/c/lf-square.nix
+++ pkgs/c/lf-square.nix
@@ -3,12 +3,12 @@ buildLinguaFranca {
     name = "lf-square";
     version = "0.1.0";
     src = fetchgit {
-        url = "https://gitea.tassilo-tanneberger.de/revol-xut/lf-add.git";
-        rev = "9a84f6429aa6b979e77ed38d76d8514f221a598b";
-        sha256 = "sha256-6AermTynIleUyXVbWNFlsYsWP6zNLzQN1WXwGsD4gTg=";
+      url = "https://gitea.tassilo-tanneberger.de/revol-xut/lf-add.git";
+      rev = "a6ddac070a729c590dff9293a6d3e40b06068df0";
+      sha256 = "sha256-RW8UvLJNQ3CE0QIGNSwL+Fau4BsJ8WEv4EH7P2jI29Y=";
     };
     language = "c";
-    mainReactor = "Main";
+    mainReactor = "SquareTest";
     buildInputs = [  ];
     meta = with lib; {
 
